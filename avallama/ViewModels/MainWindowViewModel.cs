e5d34097b2af73106ded5e2c965e1eb98e94d624
mark <mcsorgouniversity@gmail.com>
2025-01-27 20:42:14 +0100

    initial commit

---
+++ avallama/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace avallama.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avallama!";
+}
---
+++ avallama/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace avallama.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
