a67a1cd8cbba059cb6f657abd8c4e4f0c1b0d8c1
Csörgő Márk Ábrahám <167691888+eyeonspringfield@users.noreply.github.com>
2025-01-31 11:20:37 +0100

    Basic welcome page

--- avallama/ViewModels/MainWindowViewModel.cs
+++ avallama/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,10 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avallama!";
+    /*
+     * Ugye itt ezek ki lesznek cserelve ha a nyelvi beallitasok jonnek, csak ide raktam oket
+     */
+    public string Greeting { get; } = "avallama";
+    public string Version { get; } = "v0.1.0-alpha";
+    public string Info { get; } = "Run any open source LLM locally";
 }
