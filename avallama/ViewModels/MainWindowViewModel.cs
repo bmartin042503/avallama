2e41dff067305093643434b6c276c7e648e88372
bmartin042503 <bmartin042503@gmail.com>
2025-01-31 12:54:21 +0100

    added localization

--- avallama/ViewModels/MainWindowViewModel.cs
+++ avallama/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,12 @@
 ﻿namespace avallama.ViewModels;
+using avallama.Assets.Localization;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     /*
      * Ugye itt ezek ki lesznek cserelve ha a nyelvi beallitasok jonnek, csak ide raktam oket
      */
-    public string Greeting { get; } = "avallama";
+    public string Greeting { get; } = Resources.ResourceManager.GetString("Greeting_Text")!;
     public string Version { get; } = "v0.1.0-alpha";
-    public string Info { get; } = "Run any open source LLM locally";
+    public string Info { get; } = Resources.ResourceManager.GetString("Greeting_Subtext")!;
 }
