af32a6198521d86bf7aee6492779b35c2e9fad78
Csörgő Márk Ábrahám <167691888+eyeonspringfield@users.noreply.github.com>
2025-02-01 19:34:09 +0100

    added error svg, added basic ui elements, still need to recolor svg (that will take 17 hours probably, i<3.net)

--- avallama/ViewModels/HomeViewModel.cs
+++ avallama/ViewModels/HomeViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace avallama.ViewModels;
+﻿using avallama.Services;
+
+namespace avallama.ViewModels;
 
 public partial class HomeViewModel : ViewModelBase
 {
-    public string Test { get; } = "test";
+    public string LanguageLimitationWarning { get; } = LocalizationService.GetString("ONLY_SUPPORTED_MODEL");
+    public string ResourceLimitWarning { get; } = "This model may not run optimally because you have 6.9GB of VRAM, " +
+                                                  "and this model is 420GB"; //Ez hardkodolva, mert ugyis moge jon majd az uzleti logika
 }
