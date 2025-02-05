eafc1200ee2529da80e3ce31b289a949436a2962
bmartin042503 <bmartin042503@gmail.com>
2025-02-05 15:36:14 +0100

    messages scroll and window drag move changes

--- avallama/Views/HomeView.axaml.cs
+++ avallama/Views/HomeView.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -9,5 +10,16 @@ public partial class HomeView : UserControl
     public HomeView()
     {
         InitializeComponent();
+    }
+
+    private void ScrollViewer_OnScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        // Ha a görgetési terület függőlegesen növekszik (új üzenet elem) akkor legörget az aljára
+        if (e.ExtentDelta.Y > 0)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (scrollViewer == null) return;
+            scrollViewer.ScrollToEnd();
+        }
     }
 }
