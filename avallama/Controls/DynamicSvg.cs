1f1b4454975e06184eae963fd7889a7e6a842f62
bmartin042503 <bmartin042503@gmail.com>
2025-02-07 00:39:48 +0100

    custom svg control for dynamic color support + button styles

---
+++ avallama/Controls/DynamicSvg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+
+namespace avallama.Controls;
+
+// Saját Svg Control, melynek dinamikusan módosítható a színe (komplexebb SVG fájlokkal nem működhet teljesen)
+// Használatához be kell importálni a axaml-ben a Controls dir-t és <controls:DynamicSvg FillColor="">
+// FillColorban megadható bármi - pl. "Aqua", "#ff0000", "{DynamicResource OnPrimary}"
+
+public class DynamicSvg : Avalonia.Svg.Svg
+{
+    public static readonly StyledProperty<IBrush?> FillColorProperty = 
+        AvaloniaProperty.Register<DynamicSvg, IBrush?>("FillColor");
+
+    public IBrush? FillColor
+    {
+        get => GetValue(FillColorProperty);
+        set => SetValue(FillColorProperty, value);
+    }
+    
+    // public DynamicSvg(Uri baseUri) : base(baseUri) { }
+    public DynamicSvg(IServiceProvider provider) : base(provider) { }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == FillColorProperty)
+        {
+            var color = Colors.Transparent; // ebbe próbálja parsolni a megadott colort
+            var colorParse = Color.TryParse(FillColor?.ToString(), out color);
+            var rgb = color.ToUInt32(); 
+            var result = $"{rgb.ToString("x8", CultureInfo.InvariantCulture)}"; // hex-re parsolja
+            result = result.Substring(2); // az első 2 karaktert leviszi, különben nem állítható be a szín
+            if (colorParse)
+            {
+                // az svg fájlban az összes elemre beállítja az adott színt
+                // ha komplexebb, esetleg elemen belül beágyazottan van megadva az svgben a szín akkor nem írható felül jelenleg
+                SetCurrentValue(CssProperty, $"* {{ fill: #{result}; stroke: #{result} ");
+            }
+        }
+    }
+}
