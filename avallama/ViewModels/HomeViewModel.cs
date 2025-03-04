8a5de2288f4f8e6218ea04725cf50b6b2bf53c10
Csörgő Márk Ábrahám <167691888+eyeonspringfield@users.noreply.github.com>
2025-03-04 17:25:54 +0100

    flip downloaded logic because i left it reversed when testing lol

--- avallama/ViewModels/HomeViewModel.cs
+++ avallama/ViewModels/HomeViewModel.cs
@@ -66,7 +66,7 @@ public partial class HomeViewModel : PageViewModel
     private async Task CheckModelDownload()
     {
         var downloaded = await _ollamaService.IsModelDownloaded();
-        if (downloaded)
+        if (!downloaded)
         {
             IsNotDownloadedVisible = true;
         }
