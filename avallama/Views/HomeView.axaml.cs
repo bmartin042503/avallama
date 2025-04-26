4a6650ea24b8ec6f55428616d0a89e6ba8499908
Martin Bartos <bmartin042503@gmail.com>
2025-04-26 12:41:44 +0200

    sidebar resize bug fix

--- avallama/Views/HomeView.axaml.cs
+++ avallama/Views/HomeView.axaml.cs
@@ -28,6 +28,7 @@ public partial class HomeView : UserControl
     private void ScrollViewer_OnScrollChanged(object? sender, ScrollChangedEventArgs e)
     {
         // Ha a görgetési terület függőlegesen növekszik (új üzenet elem) akkor legörget az aljára
+        // ezt majd lecserélni a görgetési beállításra
         if (e.ExtentDelta.Y > 0)
         {
             var scrollViewer = sender as ScrollViewer;
@@ -42,13 +43,36 @@ public partial class HomeView : UserControl
         {
             _sideBarControl = SideBar;
             MainGrid.Children.RemoveAt(0);
-            MainGrid.ColumnDefinitions = new ColumnDefinitions("0,0,50,7*");
+            var columnDefinitions = new ColumnDefinitions
+            {
+                // sidebar
+                new ColumnDefinition(new GridLength(0, GridUnitType.Pixel)),
+                // gridsplitter
+                new ColumnDefinition(new GridLength(0, GridUnitType.Pixel)),
+                // sidebar expand/hide gomb
+                new ColumnDefinition(new GridLength(50, GridUnitType.Pixel)),
+                // chat rész
+                new ColumnDefinition(new GridLength(7, GridUnitType.Star))
+            };
+
+            MainGrid.ColumnDefinitions = columnDefinitions;
             _sideBarExpanded = false;
         }
         else
         {
             if (_sideBarControl == null) return;
-            MainGrid.ColumnDefinitions = new ColumnDefinitions("300,8,50,7*");
+            var columnDefinitions = new ColumnDefinitions
+            {
+                // sidebar
+                new ColumnDefinition(new GridLength(300, GridUnitType.Pixel)) { MinWidth = 180 },
+                // gridsplitter
+                new ColumnDefinition(new GridLength(8, GridUnitType.Pixel)),
+                // sidebar expand/hide gomb
+                new ColumnDefinition(new GridLength(50, GridUnitType.Pixel)),
+                // chat rész
+                new ColumnDefinition(new GridLength(7, GridUnitType.Star))
+            };
+            MainGrid.ColumnDefinitions = columnDefinitions;
             MainGrid.Children.Insert(0, _sideBarControl);
             _sideBarExpanded = true;
         }  
@@ -59,13 +83,19 @@ public partial class HomeView : UserControl
         // ha átméretezi a sidebart akkor beállítjuk reszponzívan az új csevegés gomb szövegét
         // mert ugye a konverter baszta átállítani
         var sideBarWidth = SideBar.Bounds.Width;
-        var buttonText = sideBarWidth switch
+        string buttonText = string.Empty;
+        switch (sideBarWidth)
         {
-            < 205 => string.Empty,
-            < 375 => LocalizationService.GetString("NEW"),
-            >= 375 => LocalizationService.GetString("NEW_CONVERSATION"),
-            _ => string.Empty
-        };
+            case < 205:
+                buttonText = string.Empty;
+                break;
+            case < 375:
+                buttonText = LocalizationService.GetString("NEW");
+                break;
+            case >= 375:
+                buttonText = LocalizationService.GetString("NEW_CONVERSATION");
+                break;
+        }
         NewConversationBtn.Content = buttonText;
     }
 }
