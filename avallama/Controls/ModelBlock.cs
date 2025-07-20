85e712b96f13a303e19956937a355ac810f1d001
Martin Bartos <bmartin042503@gmail.com>
2025-07-20 14:37:07 +0200

    modelblock styles + textlayouts

--- avallama/Controls/ModelBlock.cs
+++ avallama/Controls/ModelBlock.cs
@@ -1,70 +1,63 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using avallama.Models;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Media.Immutable;
+using Avalonia.Media.TextFormatting;
 
 namespace avallama.Controls;
 
-/*
-Template
- 
-<controls:ModelBlock
-    Title='{Binding Name}'
-    SizeText='{Binding Size}'
-    DownloadStatus='{Binding DownloadStatus}'
-    DownloadProgress='{Binding DownloadProgress}'
-    DetailItemsSource='{Binding Details}'
-    LabelItemsSource='{Binding Labels}'
-    Command='{Binding ModelCommand}'
-    CommandParameter='{Binding Index}'
-    Background='{DynamicResource Primary}'
-    Foreground='{DynamicResource OnPrimary}'
-    LabelBackground='{DynamicResource Secondary}'
-    LabelForeground='{DynamicResource OnSecondary}'/>
-*/
-
 public class ModelBlock : Control
 {
-    // AXAML Styled Propertyk
-    public static readonly StyledProperty<string> TitleProperty =
-        AvaloniaProperty.Register<ModelBlock, string>("Title");
-    
+    public static readonly StyledProperty<string?> TitleProperty =
+        AvaloniaProperty.Register<ModelBlock, string?>("Title");
+
     public static readonly StyledProperty<string> SizeTextProperty =
         AvaloniaProperty.Register<ModelBlock, string>("SizeText");
-    
+
     public static readonly StyledProperty<ModelDownloadStatus> DownloadStatusProperty =
         AvaloniaProperty.Register<ModelBlock, ModelDownloadStatus>("DownloadStatus");
-    
+
     public static readonly StyledProperty<double?> DownloadProgressProperty =
         AvaloniaProperty.Register<ModelBlock, double?>("DownloadProgress");
-    
-    public static readonly StyledProperty<IBrush> BackgroundProperty =
-        AvaloniaProperty.Register<ModelBlock, IBrush>("Background");
-    
-    public static readonly StyledProperty<IBrush> ForegroundProperty =
-        AvaloniaProperty.Register<ModelBlock, IBrush>("Foreground");
-    
-    public static readonly StyledProperty<IBrush> LabelBackgroundProperty =
-        AvaloniaProperty.Register<ModelBlock, IBrush>("LabelBackground");
-    
-    public static readonly StyledProperty<IBrush> LabelForegroundProperty =
-        AvaloniaProperty.Register<ModelBlock, IBrush>("LabelForeground");
-    
-    public static readonly StyledProperty<IEnumerable?> DetailItemsSourceProperty =
-        AvaloniaProperty.Register<ModelBlock, IEnumerable?>("DetailItemsSource");
-    
+
+    public static readonly StyledProperty<IDictionary<string, string>?> DetailItemsSourceProperty =
+        AvaloniaProperty.Register<ModelBlock, IDictionary<string, string>?>("DetailItemsSource");
+
     public static readonly StyledProperty<IEnumerable?> LabelItemsSourceProperty =
         AvaloniaProperty.Register<ModelBlock, IEnumerable?>("LabelItemsSource");
-    
+
     public static readonly StyledProperty<ICommand> CommandProperty =
         AvaloniaProperty.Register<ModelBlock, ICommand>("Command");
-    
+
     public static readonly StyledProperty<object?> CommandParameterProperty =
         AvaloniaProperty.Register<ModelBlock, object?>("CommandParameter");
 
-    public string Title
+    // tényleges stílus propertyk
+    public static readonly StyledProperty<IBrush?> BackgroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("Background");
+
+    public static readonly StyledProperty<IBrush?> ForegroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("Foreground");
+
+    public static readonly StyledProperty<IBrush?> LabelBackgroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("LabelBackground");
+
+    public static readonly StyledProperty<IBrush?> LabelForegroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("LabelForeground");
+
+    public static readonly StyledProperty<IBrush?> StrongLabelBackgroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("StrongLabelBackground");
+
+    public static readonly StyledProperty<IBrush?> StrongLabelForegroundProperty =
+        AvaloniaProperty.Register<ModelBlock, IBrush?>("StrongLabelForeground");
+
+
+    public string? Title
     {
         get => GetValue(TitleProperty);
         set => SetValue(TitleProperty, value);
@@ -88,31 +81,7 @@ public class ModelBlock : Control
         set => SetValue(DownloadProgressProperty, value);
     }
 
-    public IBrush Background
-    {
-        get => GetValue(BackgroundProperty);
-        set => SetValue(BackgroundProperty, value);
-    }
-
-    public IBrush Foreground
-    {
-        get => GetValue(ForegroundProperty);
-        set => SetValue(ForegroundProperty, value);
-    }
-    
-    public IBrush LabelBackground
-    {
-        get => GetValue(LabelBackgroundProperty);
-        set => SetValue(LabelBackgroundProperty, value);
-    }
-
-    public IBrush LabelForeground
-    {
-        get => GetValue(LabelForegroundProperty);
-        set => SetValue(LabelForegroundProperty, value);
-    }
-
-    public IEnumerable? DetailItemsSource
+    public IDictionary<string, string>? DetailItemsSource
     {
         get => GetValue(DetailItemsSourceProperty);
         set => SetValue(DetailItemsSourceProperty, value);
@@ -123,7 +92,7 @@ public class ModelBlock : Control
         get => GetValue(LabelItemsSourceProperty);
         set => SetValue(LabelItemsSourceProperty, value);
     }
-    
+
     public ICommand Command
     {
         get => GetValue(CommandProperty);
@@ -134,5 +103,157 @@ public class ModelBlock : Control
     {
         get => GetValue(CommandParameterProperty);
         set => SetValue(CommandParameterProperty, value);
+    }
+
+    public IBrush? Background
+    {
+        get => GetValue(BackgroundProperty);
+        set => SetValue(BackgroundProperty, value);
+    }
+
+    public IBrush? Foreground
+    {
+        get => GetValue(ForegroundProperty);
+        set => SetValue(ForegroundProperty, value);
+    }
+
+    public IBrush? LabelBackground
+    {
+        get => GetValue(LabelBackgroundProperty);
+        set => SetValue(LabelBackgroundProperty, value);
+    }
+
+    public IBrush? LabelForeground
+    {
+        get => GetValue(LabelForegroundProperty);
+        set => SetValue(LabelForegroundProperty, value);
+    }
+
+    public IBrush? StrongLabelBackground
+    {
+        get => GetValue(StrongLabelBackgroundProperty);
+        set => SetValue(StrongLabelBackgroundProperty, value);
+    }
+
+    public IBrush? StrongLabelForeground
+    {
+        get => GetValue(StrongLabelForegroundProperty);
+        set => SetValue(StrongLabelForegroundProperty, value);
+    }
+
+    // egyelőre égetett értékekkel, ha később igény lenne rá akkor külön styledpropertyre átvihetőek
+    // TODO: korrigálni
+    private readonly Thickness _basePadding = new(12);
+    private readonly CornerRadius _cornerRadius = new(12);
+    private const string FontFamilyName = "Manrope";
+    private const double TitleFontSize = 20;
+    private const double DetailsFontSize = 12;
+    private const double LabelFontSize = 10;
+    private const double DetailsOpacity = 0.6;
+
+    private TextLayout? _titleTextLayout;
+    private TextLayout? _detailsTextLayout;
+    private IEnumerable<TextLayout>? _labelTextLayouts;
+    private TextLayout? _sizeTextLayout;
+
+    public override void Render(DrawingContext context)
+    {
+        // Háttér renderelése
+        RenderBackground(context);
+    }
+
+    private void RenderBackground(DrawingContext context)
+    {
+        // TODO: gradientet hozzáadni?
+        context.DrawRectangle(Background, null,
+            new RoundedRect(
+                new Rect(Bounds.Size),
+                _cornerRadius
+            )
+        );
+    }
+
+    // TextLayoutok létrehozása
+    private TextLayout? CreateTitleTextLayout()
+    {
+        if (string.IsNullOrEmpty(Title)) return null;
+
+        return new TextLayout(
+            Title,
+            new Typeface(FontFamilyName),
+            null,
+            TitleFontSize,
+            Foreground ?? Brushes.Black
+        );
+    }
+
+    private TextLayout? CreateDetailsTextLayout()
+    {
+        if (DetailItemsSource == null) return null;
+
+        // egy stringbe illeszti az összes detail itemet hogy egy textlayout elemként legyen lerenderelve
+        var mergedDetailsText = string.Empty;
+        var itemCount = 0;
+        foreach (var detailItem in DetailItemsSource)
+        {
+            mergedDetailsText += $"{detailItem.Key}: {detailItem.Value}";
+            if (itemCount != DetailItemsSource.Count - 1)
+            {
+                mergedDetailsText += "\n";
+            }
+
+            itemCount++;
+        }
+
+        return new TextLayout(
+            mergedDetailsText,
+            new Typeface(FontFamilyName),
+            null,
+            DetailsFontSize,
+            new SolidColorBrush(
+                (Foreground as ImmutableSolidColorBrush)?.Color ?? Colors.Black,
+                DetailsOpacity
+            )
+        );
+    }
+
+    private IEnumerable<TextLayout>? CreateLabelTextLayouts()
+    {
+        if (LabelItemsSource is not IEnumerable<ModelLabel> labels)
+            return null;
+
+        var labelList = labels as IList<ModelLabel> ?? labels.ToList();
+        if (labelList.Count == 0)
+            return null;
+
+        List<TextLayout> layouts = [];
+        layouts.AddRange(
+            labelList.Select(
+                label => new TextLayout(
+                    label.Name, 
+                    new Typeface(FontFamilyName), 
+                    LabelFontSize, 
+                    label.Highlight == ModelLabelHighlight.Default ? LabelForeground : StrongLabelForeground
+                )
+            )
+        );
+
+        return layouts;
+    }
+    
+    private TextLayout? CreateSizeTextLayout()
+    {
+        if (string.IsNullOrEmpty(SizeText)) return null;
+
+        return new TextLayout(
+            SizeText,
+            new Typeface(FontFamilyName),
+            null,
+            DetailsFontSize,
+            new SolidColorBrush(
+                (Foreground as ImmutableSolidColorBrush)?.Color ?? Colors.Black,
+                DetailsOpacity
+            )
+        );
     }
 }
--- avallama/Models/OllamaModel.cs
+++ avallama/Models/OllamaModel.cs
@@ -1,57 +1,71 @@
+// Copyright (c) Márk Csörgő and Martin Bartos
+// Licensed under the MIT License. See LICENSE file for details.
+
+using System.Collections.Generic;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Tmds.DBus.Protocol;
 
 namespace avallama.Models;
 
-// ha nincs internet kapcsolat akkor a popular models alatt csak simán egy szöveg jelenne meg
+// NoConnectionForDownload jelezné azt ha nincs internetkapcsolat és nem letölthető a model
+// ez egyértelműnek tűnik, és talán felesleges is lenne külön minden modelblock részére beállítani ezt az állapotot
+// de arra gondoltam hogy ez így maradna ha esetleg később több forrásból (nem csak ollama) lehetne modelt letölteni
+// és ha nem elérhető a szerver a letöltéshez akkor így legalább külön választhatóak
+// tehát amit le tud tölteni azt letöltheti, amit nem az nem (de ez majd később)
 public enum ModelDownloadStatus
 {
     NotEnoughSpaceForDownload,
+    NoConnectionForDownload,
     ReadyForDownload, // ez jelöli azt ha nincs letöltve de letölthető
     Downloading,
     Downloaded
 }
 
-public enum ModelPerformanceStatus
+public enum ModelLabelHighlight
 {
-    Unknown,
-    InsufficientVram,
-    RunsOkay,
-    RunsGreat
+    Default,
+    Strong
+}
+
+public class ModelLabel(string name, ModelLabelHighlight highlight = ModelLabelHighlight.Default)
+{
+    public ModelLabelHighlight Highlight { get; set; } = highlight;
+    public string Name { get; set; } = name;
 }
 
 public partial class OllamaModel : ObservableObject
 {
+    // model neve
     [ObservableProperty] private string _name = string.Empty;
-    [ObservableProperty] private double _parameterSize; // milliardban megadva (pl. 8.0B)
-    [ObservableProperty] private int _quantizationLevel; // 4-6-8 bites, vagy 0 ami jelentené hogy nem kvantált
+    
+    // A detailshez és a labelshez lehetne hozzáadni azokat az adatokat a modelhez amik elérhetőek
+    // pl. parameters, quantization stb. dinamikusan
+    
+    // a modelblock részletei, pl. 'Parameters' '3.25B' stb.
+    // azért string hogy lehessen majd előtte lokalizált formában hozzáadni
+    [ObservableProperty] private IDictionary<string, string> _details;
+    
+    // a modelblock címkéi, Label típusokban
+    // ez azt jelentené hogy a modelhez kapcsolódó kisebb infókat pl. gyorsaság, futtathatóság kisebb címkékben jelenítené meg
+    // LabelHighlight, vagyis különböző kiemeléssel, (pl. insufficientvram kapna egy erősebb 'Strong' címkét)
+    [ObservableProperty] private IEnumerable<ModelLabel> _labels;
+    
     [ObservableProperty] private long _size; // byteokban
-    
     [ObservableProperty] private ModelDownloadStatus _downloadStatus;
-    [ObservableProperty] private double _downloadingProgress; // ha a status Downloading
-    
-    // csak ha lekérhetőek megfelelően az adatok, hogy futna-e a gépen
-    // de szerintem erre biztos van valami C# könyvtár amivel le lehet kérni hogy mennyi VRAM van
-    // és azt összehasonlítani a modellel, meg ilyesmi, de majd jobban megnézem én is
-    // ha meg bonyolult lenne akkor max mást írhatnánk ide a modelről
-    [ObservableProperty] private ModelPerformanceStatus _performanceStatus;
-    [ObservableProperty] private double _generationSpeed; // a db-ből lekérve az átlagos generálási sebesség üzenetekre
+    [ObservableProperty] private double _downloadProgress; // ha a status Downloading
 
     public OllamaModel(
         string name,
-        double parameterSize,
-        int quantizationLevel,
+        IDictionary<string, string> details,
+        IEnumerable<ModelLabel> labels,
         long size,
-        ModelDownloadStatus downloadStatus,
-        ModelPerformanceStatus performanceStatus,
-        double generationSpeed
+        ModelDownloadStatus downloadStatus
     )
     {
         Name = name;
-        ParameterSize = parameterSize;
-        QuantizationLevel = quantizationLevel;
+        Details = details;
+        Labels = labels;
         Size = size;
         DownloadStatus = downloadStatus;
-        PerformanceStatus = performanceStatus;
-        GenerationSpeed = generationSpeed;
     }
 }
