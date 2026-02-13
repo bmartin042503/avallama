48bb066d1b2ce0a830b94e6f077842fe5ecb38ac
Martin Bartos <bmartin217@proton.me>
2026-02-13 14:19:43 +0100

    added ollama process manager

---
+++ avallama.Tests/Services/OllamaProcessManagerTests.cs
@@ -0,0 +1,70 @@
+// Copyright (c) Márk Csörgő and Martin Bartos
+// Licensed under the MIT License. See LICENSE file for details.
+
+using System.Threading.Tasks;
+using avallama.Constants.States;
+using avallama.Services.Ollama;
+using avallama.Tests.Fixtures;
+using avallama.Tests.Mocks;
+using Xunit;
+
+namespace avallama.Tests.Services;
+
+public class OllamaProcessManagerTests(TestServicesFixture fixture) : IClassFixture<TestServicesFixture>
+{
+    [Fact]
+    public async Task StartAsync_SetsStartingState()
+    {
+        var opm = new OllamaProcessManager
+        {
+            StartProcessFunc = _ => new OllamaProcessMock(),
+            GetProcessesFunc = () => []
+        };
+
+        var stateChanged = false;
+        var processState = OllamaProcessState.Stopped;
+
+        opm.StatusChanged += status =>
+        {
+            if (stateChanged) return;
+            processState = status.ProcessState;
+            stateChanged = true;
+        };
+
+        await opm.StartAsync();
+
+        Assert.Equal(OllamaProcessState.Starting, processState);
+    }
+
+    [Fact]
+    public async Task StartAsync_WhenProcessIsNull_SetsNotInstalledState()
+    {
+        var opm = new OllamaProcessManager
+        {
+            StartProcessFunc = _ => null,
+            GetProcessesFunc = () => []
+        };
+
+        var processState = OllamaProcessState.Stopped;
+        opm.StatusChanged += status => processState = status.ProcessState;
+        await opm.StartAsync();
+
+        Assert.Equal(OllamaProcessState.NotInstalled, processState);
+    }
+
+    [Fact]
+    public async Task StartAsync_WithProcessRunning_SetsRunningState()
+    {
+        var opm = new OllamaProcessManager
+        {
+            StartProcessFunc = _ => new OllamaProcessMock(),
+            GetProcessesFunc = () => [ new OllamaProcessMock() ]
+        };
+
+        var processState = OllamaProcessState.Stopped;
+        opm.StatusChanged += status => processState = status.ProcessState;
+        await opm.StartAsync();
+
+        Assert.Equal(OllamaProcessState.Running, processState);
+    }
+}
---
+++ avallama/Constants/States/OllamaProcessState.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Márk Csörgő and Martin Bartos
+// Licensed under the MIT License. See LICENSE file for details.
+
+namespace avallama.Constants.States;
+
+/// <summary>
+/// Defines the lifecycle states of the local Ollama executable process.
+/// </summary>
+public enum OllamaProcessState
+{
+    /// <summary>
+    /// The process is currently starting up.
+    /// </summary>
+    Starting,
+
+    /// <summary>
+    /// The process is actively running.
+    /// </summary>
+    Running,
+
+    /// <summary>
+    /// The process has been stopped or has not been started yet.
+    /// </summary>
+    Stopped,
+
+    /// <summary>
+    /// The process failed to start or exited unexpectedly.
+    /// </summary>
+    Failed,
+
+    /// <summary>
+    /// The Ollama executable was not found on the system.
+    /// </summary>
+    NotInstalled
+}
